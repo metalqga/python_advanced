87f1be1abe0ddd980eb2ca1bac51f8cc1cab1fa0
Stoyan Hristov <88736883+metalqga@users.noreply.github.com>
2022-02-21 23:43:47 +0200

    Delete 20.homework directory

--- 20.homework/1.py
+++
@@ -1,8 +0,0 @@
-punctuation = {"-", ",", ".", "!", "?"}
-with open("text.txt") as file:
-    for row, line in enumerate(file):
-        if row % 2 == 0:
-            reversed_line = " ".join(reversed(line.strip().split()))
-            for ch in punctuation:
-                reversed_line = reversed_line.replace(ch, "@")
-            print(reversed_line)
